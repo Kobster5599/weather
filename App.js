7b1064193a73165d6102c76027c8a06f16cfad0a
Kobe Puskas <Puskaskobe@gmail.com>
2023-01-30 18:46:59 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { useFonts } from 'expo-font';
+
+import Icon from './weatherIcon';
+
+import WeatherDetail from './component/WeatherDetail';
+import WeatherHourly from './component/WeatherHourly';
+import WeatherSummary from './component/WeatherSummary';
 
 export default function App() {
+
+  const [loaded] = useFonts(
+    { icomoon: require('./assets/fonts/icomoon.ttf'), }
+  );
+
+  if(!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView style={styles.container}>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+
+      <WeatherSummary/>
+
+      <Icon name="wi-cloudy-gusts"  style={styles.icon}/>
+
+
+      <WeatherDetail/>
+
+
+      <WeatherHourly/>
+
+      
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    //alignItems: 'center',
+   //justifyContent: 'center',
   },
+  text: {
+    color: '#fff'
+  },
+  icon: {
+    color: 'lightblue',
+    fontSize: 74
+  }
 });
